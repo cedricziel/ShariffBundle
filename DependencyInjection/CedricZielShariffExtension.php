d8a66662aa5729a393da58fc7ae39f212abd3419
Cedric Ziel <cedric@cedric-ziel.com>
2016-05-13 12:00:33 +0200

    Use correct configuration namespace

--- DependencyInjection/CedricZielShariffExtension.php
+++ DependencyInjection/CedricZielShariffExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class ValitonShariffExtension extends Extension
+class CedricZielShariffExtension extends Extension
 {
     /**
      * {@inheritDoc}
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('valiton_shariff');
+        $rootNode = $treeBuilder->root('cedricziel_shariff');
 
         $rootNode
             ->children()
